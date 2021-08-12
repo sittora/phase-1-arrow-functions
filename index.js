aaa29a1729d4984f2fb9fa0a913d991628aae701
sittora <sittora@gmail.com>
2021-08-11 21:01:14 -0400

    commit tree 794fea90d4d7e808c8929078f2ad0db6dbe565a8

---
+++ index.js
@@ -0,0 +1,15 @@
+
+function divide(a, b) {
+    return (a/b);
+  }
+
+
+function divide(a, b) {
+    return (2000 / 100);
+  }
+
+  let square = (a) => { 
+      return a*a
+    } 
+
+    let add = (a, b) => a + b
